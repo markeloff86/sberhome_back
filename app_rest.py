e9b9ce7d595b7fff642f93398ad118fdb98cce56
SIGMA\sbt-kiyashchenko-io <kio_1107@mail.ru>
2018-11-02 03:27:57 +0600

    fixed host

--- app_rest.py
+++ app_rest.py
@@ -68,5 +68,5 @@ def is_fail():
 
 
 if __name__ == '__main__':
-    # app.run(debug=True, host='192.168.43.150')
-    app.run(debug=True, host='127.0.0.1')
+    app.run(debug=True, host='192.168.43.150')
+    # app.run(debug=True, host='127.0.0.1')
