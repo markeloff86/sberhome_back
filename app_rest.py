c07a92e84e0af77bc5e2c48016cffed927e61d93
Nikolay Klyuev <nklyuev@example.com>
2018-11-02 00:49:40 +0600

    Добавил работу с БД

--- app_rest.py
+++ app_rest.py
@@ -2,6 +2,7 @@
 
 import random
 import uuid
+import sqlite3
 
 from flask import Flask, jsonify
 from flask_cors import CORS
@@ -9,6 +10,15 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 app.config['JSON_AS_ASCII'] = False
+
+
+def get_last_active(param):
+    conn = sqlite3.connect('main.db')
+    cursor = conn.cursor()
+    sql = "SELECT " + param + " FROM challenges"
+    cursor.execute(sql)
+    print(cursor.fetchone()[0])
+    return cursor.fetchone()[0]
 
 
 # Заглушки. Эмулириют СБОЛ, возвращают название копилки, описание и кол-во денег
@@ -22,7 +32,7 @@ def get_info():
 
 
 def get_cash():
-    return ['0', '100']
+    return [0, 100]
 
 
 @app.route('/pushNotification', methods=['GET', 'POST'])
@@ -49,6 +59,13 @@ def get_cli_commands():
     return jsonify(smart_box=boxes_list)
 
 
+@app.route('/checkDate', methods=['GET', 'POST'])
+def date_compare():
+    time_one = get_last_active("last_update_time")
+    time_two = get_last_active("fail_time")
+    return "last_update_time - " + time_two + " | exception time - " + time_one
+
+
 if __name__ == '__main__':
-    app.run(debug=True, host='192.168.43.150')
-    # app.run(debug=True, host='127.0.0.1')
+    # app.run(debug=True, host='192.168.43.150')
+    app.run(debug=True, host='127.0.0.1')
--- sql/sql.sql
+++ sql/sql.sql
@@ -2,19 +2,20 @@
 -- @name: название челенджа,
 -- @status:  0 - не активен, 1 - активен
 -- @cost - стоимсоть штрафа
--- @time -  дата последней активации. format: YYYY-MM-DD HH:MM:SS.SSS
+-- @time -  дата последней активации. format: HH:MM:SS
 CREATE TABLE challenges (
-	id INTEGER PRIMARY KEY NOT NULL,
-	name TEXT NOT NULL,
-	status INTEGER NOT NULL,
-	cost INTEGER NOT NULL,
-	fail_time TEXT NOT NULL,
-	last_update_time TEXT NOT NULL
+    id INTEGER PRIMARY KEY NOT NULL,
+    name TEXT NOT NULL,
+    status INTEGER NOT NULL,
+    cost INTEGER NOT NULL,
+    fail_time TEXT NOT NULL,
+    last_update_time TEXT NOT NULL
 );
 
 -- Заполнение БД
 INSERT INTO challenges(id, name, status, cost, fail_time, last_update_time)
-VALUES (1, "Ограничение ночного холодильника", 1, 100, "23:00:00", "2018-11-01 23:00:05.123");
+VALUES (1, "Контроль потребления электричества", 0, 100, "01:00", "23:00");
 
 INSERT INTO challenges(id, name, status, cost, fail_time, last_update_time)
-VALUES (2, "Контроль потребления электричества", 0, 100, "01:00:00", "2018-11-01 23:00:05.123");
+VALUES (2, "Ограничение ночного холодильника", 1, 100, "23:00", "23:00");
+
