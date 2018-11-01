e59f0ea7d0f96118993123f7e1ed52f0baa4a9ce
markeloff86 <markeloffds@gmail.com>
2018-11-02 00:31:58 +0600

    has been added publication for motion

--- app_mqtt.py
+++ app_mqtt.py
@@ -9,13 +9,8 @@ def on_connect(client, userdata, flags, rc):
     print("Connected with result code " + str(rc))
 
 
-def on_message(client, userdata, msg):
-    print(msg.topic + " " + str(msg.payload))
-
-
 client = mqtt.Client()
 client.on_connect = on_connect
-client.on_message = on_message
 
 client.username_pw_set("mqtt_client", "sberhack")
 client.connect("192.168.43.150", 1883, 60)
