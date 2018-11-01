9e94f01938d35d7a92cf001da83f03c9b2cae212
SIGMA\sbt-kiyashchenko-io <kio_1107@mail.ru>
2018-11-01 15:43:24 +0600

    added getRecommendationsJsonName() and createRecommendations()

--- app_rest.py
+++ app_rest.py
@@ -1,13 +1,26 @@
 from flask import Flask
 from flask_cors import CORS
+import json
 
 app = Flask(__name__)
 CORS(app)
 
 
+def getRecommendationsJsonName():
+    return 'resources/recommendations.json'
+
+
+def createRecommendations(jsonStr):
+    encoder = json.JSONEncoder()
+    with open(getRecommendationsJsonName(), 'w') as recJsonFile:
+        recJsonFile.write(encoder.encode(jsonStr))
+        return recJsonFile
+
+
 @app.route('/home/v1.0/recommendations')
 def get_motions():
-    return "Pes"
+    jsonRecommendations = createRecommendations({"foo": ["bar", "baz"]})
+    return 'TODO'
 
 
 if __name__ == '__main__':
