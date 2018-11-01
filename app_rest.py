ee13eef92714a6fa2990b77fb400fa56cb9fc0a2
Nikolay Klyuev <nklyuev@example.com>
2018-11-01 16:31:36 +0600

    add RPI IP

--- app_rest.py
+++ app_rest.py
@@ -31,4 +31,5 @@ def getJson():
     return jsonify(data)
 
 if __name__ == '__main__':
-    app.run(debug=True, host='127.0.0.1')
+    # app.run(debug=True, host='127.0.0.1')
+    app.run(debug=True, host='192.168.43.150')
