5f9f1b81b142547ccd9893c84444a8e4cef30fb4
Harmattan322 <kio_1107@mail.ru>
2019-05-25 14:49:02 +0600

    new client.connect

--- motion.py
+++ motion.py
@@ -2,8 +2,6 @@ import RPi.GPIO as GPIO
 import time
 import paho.mqtt.client as mqtt
 import json
-import db_helper
-from datetime import datetime
 
 
 def on_connect(client, userdata, flags, rc):
@@ -13,8 +11,8 @@ def on_connect(client, userdata, flags, rc):
 client = mqtt.Client()
 client.on_connect = on_connect
 
-client.username_pw_set("mqtt_client", "sberhack")
-client.connect("192.168.43.150", 1883, 60)
+# client.username_pw_set("mqtt_client", "pass")
+client.connect("18.223.169.60", 1883, 60)
 client.loop_start()
 GPIO.setmode(GPIO.BCM)
 GPIO_PIR = 4
@@ -29,7 +27,6 @@ try:
         if Current_State == 1 and Previous_State == 0:
             motion = {"motionDetected": True}
             client.publish("motion", json.dumps(motion))
-            db_helper.update_challenges("last_update_time", datetime.strftime(datetime.now(), "%H:%M"))
             time.sleep(5)
             Previous_State = 1
         elif Current_State == 0 and Previous_State == 1:
